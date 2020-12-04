d27c2a747dc22433a9444ef9d16eeaa7a1227681
moxy <moxy7@qq.com>
2020-12-04 16:08:10 +0800

    12.4

--- winter Js课程/Week-12/jsx/main.js
+++ winter Js课程/Week-12/jsx/main.js
@@ -15,10 +15,73 @@ class Carousel extends Component {
         for(let record of this.attributes.src) {    // 利用let of把照片地址添加到div子元素上 
             let child = document.createElement("div");
             child.style.backgroundImage = `url('${record}')`;    //添加地址到backgroundImage，字符串要转义。
-            child.style.display = "none";     // 照片默认是不显示的，轮到谁显示谁。
             
             this.root.appendChild(child);
         }
+
+        // 鼠标按下拖动时的相对位置。[0, 1, 2, 3] 是有效位置，没有设置截断值。
+        let position = 0;
+        // 鼠标拖动功能
+        this.root.addEventListener("mousedown", event => {
+            let children = this.root.children;
+            // 鼠标的初始位置。只看x放心即可，y不用考虑。
+            let startX = event.clientX;
+
+            let move = event => {
+                // 鼠标移动的距离。
+                let x = event.clientX - startX;
+                for(let child of children) {
+                    child.style.transition = "none";
+                    child.style.transform = `translate(${- position * 500 + x}px)`;
+                }
+            }
+
+            let up = event => {
+                let x = event.clientX - startX;
+
+                // 如果拖够了一半的位置，就判断为拖到下一张图片；否则就是上一张图片。
+                // round取近似值。
+                position = position - Math.round( x / 500);
+
+                for(let child of children) {
+                    child.style.transition = "";
+                    child.style.transform = `translate(${- position * 500}px)`;
+                }
+                document.removeEventListener("mousemove", move);
+                document.removeEventListener("mouseup", up);
+            }
+
+            document.addEventListener("mousemove", move); 
+            document.addEventListener("mouseup", up); 
+        }); 
+
+
+
+
+
+
+        // 自动播放功能
+        // let currentIndex = 0;
+        // setInterval(() => {
+        //     let children = this.root.children;
+        //     let nextIndex = (currentIndex + 1) % children.length; 
+            
+        //     let current = children[currentIndex];
+        //     let next = children[nextIndex];
+
+        //     next.style.transition = "none";    // 做这个移动的时候，不希望有动画，要立刻执行。
+        //     next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
+
+        //     // why use setTimeout? 
+        //     setTimeout(() => {
+        //         next.style.transition = "";        // 取消“none”的限制，恢复css原本的控制权。
+        //         current.style.transform = `translateX(${-100 - currentIndex * 100}%)`;
+        //         next.style.transform = `translateX(${- nextIndex * 100}%)`;
+
+        //         currentIndex = nextIndex;
+        //     }, 16);
+        // }, 3000);
+
         return this.root;
     }
     mountTo(parent){
