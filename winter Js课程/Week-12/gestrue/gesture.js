55111046087bddcc70e1a2d1726512691f7d0ed0
moxy <moxy7@qq.com>
2020-12-10 22:21:04 +0800

    12.10, week 14, component, class 6

--- winter Js课程/Week-12/gestrue/gesture.js
+++ winter Js课程/Week-12/gestrue/gesture.js
@@ -2,6 +2,8 @@
 // 取HTML元素
 let element = document.documentElement;
 
+let isListeningMouse = false;
+
 // mouse事件
 element.addEventListener("mousedown", event => {
     
@@ -33,12 +35,19 @@ element.addEventListener("mousedown", event => {
         let context = contexts.get("mouse" + (1 << event.button));
         end(event, context);
         contexts.delete("mouse" + (1 << event.button));
-        element.removeEventListener("mousemove", mousemove);
-        element.removeEventListener("mouseup", mouseup);
+
+        if(event.buttons === 0) {
+            document.removeEventListener("mousemove", mousemove);
+            document.removeEventListener("mouseup", mouseup);
+            isListeningMouse = false;
+        }
     };
 
-    element.addEventListener("mousemove", mousemove);
-    element.addEventListener("mouseup", mouseup);
+    if(!isListeningMouse) {
+        document.addEventListener("mousemove", mousemove);
+        document.addEventListener("mouseup", mouseup);
+        isListeningMouse = true;
+    }
 });
 
 let contexts = new Map();
@@ -75,10 +84,16 @@ element.addEventListener("touchcancel", event => {
 let start = (point, context) => {
     // console.log("start", point.clientX, point.clientY);
     context.startX = point.clientX, context.startY = point.clientY;
+    context.points = [{
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY,
+    }];
 
     context.isTap = true;
     context.isPan = false;
     context.isPress = false;
+    context.isFlick = false;
 
     // 监控：0.5s事件
     context.handler = setTimeout(() => {
@@ -102,28 +117,74 @@ let move = (point, context) => {
         clearTimeout(context.handler);
     }
     if(context.isPan) {
-        console.log(dx,dy);
-        console.log("pan");
+        // console.log(dx,dy);
+        // console.log("pan");
     }
+
+    context.points = context.points.filter(point => Date.now() - point.t < 500);
+    context.points.push({
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY,
+    });
     // console.log("move", point.clientX, point.clientY);
 }
 
 let end = (point, context) => {
     if(context.isTap) {
-        console.log("tap");
+        // console.log("tap");
+        dispatch("tap", {});
         clearTimeout(context.handler);
     }
     if(context.isPan) {
-        console.log("panend");
+        // console.log("panend");
+        dispatch("pan",{
+            clientX: point.clientX,
+            clientY: point.clientY,
+        });
     }
     if(context.isPress) {
-        console.log("pressend");
+        // console.log("pressend");
+        dispatch("press",{
+            clientX: point.clientX,
+            clientY: point.clientY,
+        });
     }
-    console.log("end", point.clientX, point.clientY);
-    console.log(point);
+    
+    context.points = context.points.filter(point => Date.now() - point.t < 500);
+    let d, v;
+    if(!context.points.length) {
+        v = 0;
+    } else {
+        d = Math.sqrt((point.clientX - context.points[0].x) ** 2 + 
+            (point.clientY - context.points[0].y) ** 2);
+        v = d / (Date.now() - context.points[0].t);
+    }
+
+    if (v > 1.5) {
+        // console.log("flick");
+        context.isFlick = true;
+        dispatch("flick",{
+            clientX: point.clientX,
+            clientY: point.clientY,
+        });
+    } else {
+        context.isFlick = false;
+    }
+
+    // console.log(v);
+    // console.log("end", point.clientX, point.clientY);
 }
 
 let cancel = (point, context) => {
     clearTimeout(context.handler);
     console.log("cancel", point.clientX, point.clientY);
+}
+
+function dispatch(type, properties) {
+    let event = new Event(type);
+    for (let name in properties) {
+        event[name] = properties[name];
+    }
+    element.dispatchEvent(event);
 }
