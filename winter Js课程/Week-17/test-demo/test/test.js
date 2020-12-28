e00e7b7adab2194e8c522a18073573c0094079b7
moxy <moxy7@qq.com>
2020-12-28 14:33:01 +0800

    Week16  | 补充笔记内容

--- winter Js课程/Week-17/test-demo/test/test.js
+++ winter Js课程/Week-17/test-demo/test/test.js
@@ -1,20 +1,20 @@
-var assert = require('assert');
+// var assert = require('assert');
 
-import {add, mul} from "../add.js";
+// import {add, mul} from "../add.js";
 
-// var add = require('../add.js').add;
-// var mul = require('../add.js').mul;
+// // var add = require('../add.js').add;
+// // var mul = require('../add.js').mul;
 
-describe("add function testing", function(){
-    it('1 + 2 shoule be 3', function() {
-        assert.equal(add(1,2), 3);
-    }); 
+// describe("add function testing", function(){
+//     it('1 + 2 shoule be 3', function() {
+//         assert.equal(add(1,2), 3);
+//     }); 
 
-    it('-1 + -6 shoule be -7', function() {
-        assert.equal(add(-1,-6), -7);
-    }); 
+//     it('-1 + -6 shoule be -7', function() {
+//         assert.equal(add(-1,-6), -7);
+//     }); 
 
-    it('-2 * -6 shoule be 12', function() {
-        assert.equal(mul(-2,-6), 12);
-    }); 
-})
+//     it('-2 * -6 shoule be 12', function() {
+//         assert.equal(mul(-2,-6), 12);
+//     }); 
+// })
