93169346129ee91d96d981fcb7614a7daf1b36df
moxy <moxy7@qq.com>
2020-12-30 16:28:51 +0800

    week 18 | 发布系统 | 12.30 | day 3 | done

--- winter Js课程/Week-18/publish-server/server.js
+++ winter Js课程/Week-18/publish-server/server.js
@@ -1,17 +1,86 @@
 let http = require('http');
-let fs = require('fs');
+let https = require('https');
+let unzipper = require('unzipper');
+let querystring = require('querystring');
+
+// 2. auth路由：接受code，用code+client_id+client_secret换token
+function auth(request, response) {
+    let query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1]);
+    getToken(query.code, function(info){
+        // response.write(JSON.stringify(info));
+        response.write(`<a href='http://localhost:8083/?token=${info.access_token}'>publish</a>`)
+        response.end();
+    });
+}
+
+function getToken(code, callback) {
+    let request = https.request({
+        hostname: "github.com",
+        path: `/login/oauth/access_token?code=${code}&client_id=Iv1.5da408d36319cfb8&client_secret=2e040ca73e13942ac121e135208dddc616db82f9`,
+        port: 443,
+        method: "POST"
+    }, function(response) {
+        let body = "";
+        response.on('data', chunk => {
+            body += (chunk.toString());
+        });
+        response.on('end', chunk => {
+            callback(querystring.parse(body));
+        });
+    });
+    request.end();
+}
+
+
+function publish(request, response) {
+
+}
+// 4. publish路由：用token获取用户信息，检查权限，接受发布
+function publish(request, response) {
+    let query = querystring.parse(request.url.match(/^\/publish\?([\s\S]+)$/)[1]);
+
+    getUser(query.token, info => {
+        if(info.login === "MoxyNJ") {
+            request.pipe(unzipper.Extract({ path: '../server/public/' }));
+            request.on('end', function(){
+                response.end('success!');
+            })
+        }
+    });    
+    // https://api.github.com/user
+}
+
+function getUser(token, callback) {
+    let request = https.request({
+        hostname: "api.github.com",
+        path: `/user`,
+        port: 443,
+        method: "GET",
+        headers: {
+            Authorization: `token ${token}`,
+            "User-Agent": 'toy-publish'
+        }
+    }, function(response) {
+        let body = "";
+        response.on('data', chunk => {
+            body += (chunk.toString());
+        });
+        response.on('end', chunk => {
+            callback(JSON.parse(body));
+        });
+    });
+    request.end();
+}
+
 
 http.createServer(function(request, response){
-    console.log(request.headers);
+    if(request.url.match(/^\/auth\?/))
+        return auth(request, response);
+    if(request.url.match(/^\/publish\?/))
+        return publish(request, response);
+    // console.log(request.headers);
 
-    let outFile = fs.createWriteStream("../server/public/index.html");
-    
+    // let outFile = fs.createWriteStream("../server/public/tmp.zip");    
 
-    request.on('data', chunk => {
-        outFile.write(chunk);
-    });
-    response.on('end', () => {
-        outFile.end();
-        console.log("Success");
-    });
+
 }).listen(8082);
--- winter Js课程/Week-18/publish-tool/publish.js
+++ winter Js课程/Week-18/publish-tool/publish.js
@@ -1,25 +1,45 @@
 let http = require("http");
 let fs = require("fs");
+let archiver = require("archiver");
+let child_process = require("child_process");
+let querystring = require("querystring");
 
-let request = http.request({
-    hostname:"127.0.0.1",
-    port: 8882,
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/octet-stream'
-    }
-}, response => { 
-    console.log(response);
-});
+// 1. 打开 https://github.com/login/oauth/authorize
+child_process.exec(`open https://github.com/login/oauth/authorize?client_id=Iv1.5da408d36319cfb8`)
 
 
-let file = fs.createReadStream("./sample.html");
 
-file.on('data', chunk => {
-    console.log(chunk.toString());
-    request.write(chunk);
-})
-file.on('end', chunk => {
-    console.log("read finished");
-    request.end(chunk);
-})
+// 3. 创建server，接受token，然后点击发布
+http.createServer(function (request, response) {
+    let query = querystring.parse(request.url.match(/^\/\?([\s\S]+)$/)[1]);
+    publish(query.token);
+}).listen(8083);
+
+function publish(token) {
+    let request = http.request({
+        hostname: "127.0.0.1",
+        port: 8082,
+        method: "POST",
+        path:"/publish?token=" + token,
+        headers: {
+            'Content-Type': 'application/octet-stream'
+            // "Content-length": stats.size
+        }
+    }, response => {
+        console.log(response);
+    });
+
+    // let file = fs.createReadStream("./sample.html");
+
+
+    const archive = archiver('zip', {
+        zlib: {
+            level: 9
+        }
+    });
+
+    archive.directory('./sample/', false);
+    archive.finalize();
+
+    archive.pipe(request);
+}
